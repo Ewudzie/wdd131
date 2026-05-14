9d98bc659d58b458e482e0fe94271684d18cc904
Ewudzie <eewudzie1@byupathway.edu>
2026-05-14 19:12:40 +0000

    update on web resources lits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,8 @@
-document.getElementById("lastModified").innerHTML = document.lastModified;
+//document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// Update the copyright year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Show the page's actual last modified date and time
+document.getElementById("lastModified").textContent =
+    "Last Modification: " + document.lastModified;
