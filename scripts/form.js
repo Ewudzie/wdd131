069728a6d865f926983f6801b23fb72484615c24
Ewudzie <eewudzie@byupathway.edu>
2026-06-08 13:09:01 +0000

    worked on forms assignment for  week05

--- scripts/form.js
+++ scripts/form.js
@@ -1,29 +1,29 @@
 const products = [
-    {
-        id: "fc-1888",
-        name: "FC-1888",
-        averageRating: 4.5
-    },
-    {
-        id: "fc-2050",
-        name: "FC-2050",
-        averageRating: 4.7
-    },
-    {
-        id: "fs-1987",
-        name: "FS-1987",
-        averageRating: 3.5
-    },
-    {
-        id: "ac-2000",
-        name: "AC-2000",
-        averageRating: 4.9
-    },
-    {
-        id: "jj-1969",
-        name: "JJ-1969",
-        averageRating: 3.9
-    }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
 const productSelect = document.querySelector("#product-name");
